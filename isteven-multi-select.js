9f3d265c750d26d3577b8abc641a8d23f6f79ede
oualid.lahkim <oualid.lahkim@kronnos>
2018-04-06 11:45:57 -0400

    support label formatting

--- isteven-multi-select.js
+++ isteven-multi-select.js
@@ -43,6 +43,7 @@ angular.module( 'isteven-multi-select', ['ng'] ).directive( 'istevenMultiSelect'
             // models
             inputModel      : '=',
             outputModel     : '=',
+            getOutputLabel  : '=',
 
             // settings based on attribute
             isDisabled      : '=',
@@ -535,27 +536,31 @@ angular.module( 'isteven-multi-select', ['ng'] ).directive( 'istevenMultiSelect'
                     }
                     else {
                         $scope.more = false;
-                    }                
-                    
-                    angular.forEach( $scope.inputModel, function( value, key ) {
-                        if ( typeof value !== 'undefined' && value[ attrs.tickProperty ] === true ) {                        
-                            if ( ctr < tempMaxLabels ) {                            
-                                $scope.varButtonLabel += ( $scope.varButtonLabel.length > 0 ? '</div>, <div class="buttonLabel">' : '<div class="buttonLabel">') + $scope.writeLabel( value, 'buttonLabel' );
-                            }
-                            ctr++;
-                        }
-                    });                
+                    }
 
-                    if ( $scope.more === true ) {
-                        // https://github.com/isteven/angular-multi-select/pull/16
-                        if (tempMaxLabels > 0) {
-                            $scope.varButtonLabel += ', ... ';
-                        }
-                        $scope.varButtonLabel += '(' + $scope.outputModel.length + ')';                        
+                    if (typeof $scope.getOutputLabel === 'function') {
+											$scope.varButtonLabel = $scope.getOutputLabel($scope.outputModel.length);
+										} else {
+											angular.forEach( $scope.inputModel, function( value, key ) {
+												if ( typeof value !== 'undefined' && value[ attrs.tickProperty ] === true ) {
+													if ( ctr < tempMaxLabels ) {
+														$scope.varButtonLabel += ( $scope.varButtonLabel.length > 0 ? '</div>, <div class="buttonLabel">' : '<div class="buttonLabel">') + $scope.writeLabel( value, 'buttonLabel' );
+													}
+													ctr++;
+												}
+											});
+
+											if ( $scope.more === true ) {
+												// https://github.com/isteven/angular-multi-select/pull/16
+												if (tempMaxLabels > 0) {
+													$scope.varButtonLabel += ', ... ';
+												}
+												$scope.varButtonLabel += '(' + $scope.outputModel.length + ')';
+											}
                     }
                 }
                 $scope.varButtonLabel = $sce.trustAsHtml( $scope.varButtonLabel + '<span class="caret"></span>' );                
-            }
+            };
 
             // Check if a checkbox is disabled or enabled. It will check the granular control (disableProperty) and global control (isDisabled)
             // Take note that the granular control has higher priority.
